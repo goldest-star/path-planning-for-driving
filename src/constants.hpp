c3bea215153b9798905e07c1bff37920acacd04c
path-planning-for-driving <cristiano900520@gmail.com>
2020-04-12 11:16:53 +0200

    fix: Fixed jerk (#6)
    Extended front and rear margins, reduced incremental acceleration and raised spline distance

--- src/constants.hpp
+++ src/constants.hpp
@@ -2,17 +2,17 @@
 
 namespace constants {
 // Distance in meters between points that will be interpolated using spline
-constexpr float SPLINE_DIST = 30;
+constexpr float SPLINE_DIST = 40;
 // Target velocity (mph)
 constexpr double TARGET_VELOCITY = 49.9;
 // Velocity step (mph)
-constexpr double VELOCITY_STEP = 0.7;
+constexpr double VELOCITY_STEP = 0.5;
 // Refresh period in seconds
 constexpr double REFRESH = .02;  // 50Hz
 // Lane width in meters
 constexpr float LANE_WIDTH = 4;
 // Margin in meters with vehicle ahead before action in required
-constexpr double FRONT_MARGIN = 30;
+constexpr double FRONT_MARGIN = 40;
 // Margin in meters with vehicle behind before action in required
-constexpr double REAR_MARGIN = 5;
+constexpr double REAR_MARGIN = 10;
 }  // namespace constants
